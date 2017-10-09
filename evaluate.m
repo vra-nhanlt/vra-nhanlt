ede9223229a457c501e6d8691779f4fb44ddaa23
trongnhanuit <trongnhan.uit@gmail.com>
2017-10-10 00:15:18 +0700

    Update BaiTap ThucHanh 3: - Add total_correct_results_count to the outputs of the function evaluate

--- evaluate.m
+++ evaluate.m
@@ -1,7 +1,8 @@
-function [average_recall, average_precision] = evaluate(trainingImages, trainingLabels, testingImages, testingLabels, distance, k)
+function [total_correct_results_count, average_recall, average_precision] = evaluate(trainingImages, trainingLabels, testingImages, testingLabels, distance, k)
 cufusion_matrix = confusion_matrix_custom_model(trainingImages, trainingLabels, testingImages, testingLabels, distance, k)
  
 % average recall; average precision
+total_correct_results_count = 0;
 sum_recall = 0;
 sum_precision = 0;
 label_max = 9;
@@ -13,6 +14,7 @@ for (i = 0: label_max)
         all_returned_result_count = all_returned_result_count + cufusion_matrix((j + 2),(i + 2));
     end
     returned_correct_result_count = cufusion_matrix((i + 2), (i + 2));
+    total_correct_results_count = total_correct_results_count + returned_correct_result_count;
     
     current_recall = returned_correct_result_count / all_correct_result_count;
     sum_recall = sum_recall + current_recall;
@@ -21,6 +23,7 @@ for (i = 0: label_max)
     sum_precision = sum_precision + current_precision;
 end
  
+total_correct_results_count
 average_recall = sum_recall/(label_max + 1)
 average_precision = sum_precision/(label_max + 1)
 end
